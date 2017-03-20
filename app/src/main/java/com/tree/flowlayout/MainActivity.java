6b77509eb754fa97e4b026f8db0f5ebe104e6138
talha629 <talha629@gmail.com>
2017-03-20 17:53:24 +0600

    Initial commit

---
+++ app/src/main/java/com/tree/flowlayout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.tree.flowlayout;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = new SingleChooseFragment();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_holder, fragment)
+                .commit();
+    }
+}
